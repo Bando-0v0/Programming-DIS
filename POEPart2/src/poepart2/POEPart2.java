611d5f2c679991275ab764923547db78053ed558
Bando-0v0 <163292321+Bando-0v0@users.noreply.github.com>
2024-05-22 13:40:20 +0000

    POE part 2

---
+++ POEPart2/src/poepart2/POEPart2.java
@@ -0,0 +1,13 @@
+
+package poepart2;
+
+
+public class POEPart2 {
+
+
+    public static void main(String[] args) {
+       taskStorage myObj1 = new taskStorage();
+       myObj1.registerUser();
+    }
+    
+}
