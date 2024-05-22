18332c2f41f972cdb36bb5407169129a95e88a35
Bando-0v0 <163292321+Bando-0v0@users.noreply.github.com>
2024-05-22 20:42:59 +0200

    Add files via upload

---
+++ POEPart2/progPoePart2/src/progpoepart2/ProgPoePart2.java
@@ -0,0 +1,13 @@
+
+package progpoepart2;
+import javax.swing.JOptionPane;
+
+public class ProgPoePart2 {
+
+    
+    public static void main(String[] args) {
+        taskStorage myObj1 = new taskStorage();
+        myObj1.registerUser();
+    }
+    
+}
